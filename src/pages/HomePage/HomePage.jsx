4f74d7b10fee5f9a741286ca2b053cdcd24d70f1
HnatenkoOlya <gnatenkoolgaig1210@gmail.com>
2025-12-31 22:47:58 +0100

    1/2

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,18 +1,28 @@
+import { Link } from "react-router";
 import css from "./HomePage.module.css";
 function HomePage() {
   return (
     <div className={css.divHome}>
-      <h1 className={css.title}>
-        The road to the <span className={css.span}>depths</span> of the human
-        soul
-      </h1>
-      <p className={css.p}>
-        We help you to reveal your potential, overcome challenges and find a
-        guide in your own life with the help of our experienced psychologists.
-      </p>
-      <a href="/psychologists" className={css.link}>
-        Get started
-      </a>
+      <div className={css.divContent}>
+        <h1 className={css.title}>
+          The road to the <span className={css.span}>depths</span> of the human
+          soul
+        </h1>
+        <p className={css.p}>
+          We help you to reveal your potential, overcome challenges and find a
+          guide in your own life with the help of our experienced psychologists.
+        </p>
+        <Link to="/psychologists" className={css.link}>
+          Get started
+        </Link>
+      </div>
+      <div className={css.divImage}>
+        <img
+          src="/images/hero/heroImg1x.jpg"
+          srcSet="/images/hero/heroImg2x.jpg 2x"
+          alt="Psychologist"
+        />
+      </div>
     </div>
   );
 }
