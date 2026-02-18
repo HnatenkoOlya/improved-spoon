31e2a9c6e121ebabdd36cebd383b5303a4251c88
HnatenkoOlya <gnatenkoolgaig1210@gmail.com>
2026-02-18 23:24:33 +0100

    4/1

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -4,13 +4,13 @@ import { getDatabase } from "firebase/database";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAGZu05T1rIgWAHoELUFQApVkb0lWMnn8I",
-  authDomain: "psychologist-services-78dcf.firebaseapp.com",
-  projectId: "psychologist-services-78dcf",
-  storageBucket: "psychologist-services-78dcf.firebasestorage.app",
-  messagingSenderId: "255607756779",
-  appId: "1:255607756779:web:d79091d78348db52006bae",
-  measurementId: "G-5WVF1BZH7H",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
