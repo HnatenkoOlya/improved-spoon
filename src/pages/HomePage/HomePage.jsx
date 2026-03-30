994fe5a4ed51166c3add89f6493920041635911b
HnatenkoOlya <gnatenkoolgaig1210@gmail.com>
2026-03-30 20:02:42 +0200

    add QR

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,7 +1,19 @@
 import { Link } from "react-router";
 import arrowIcon from "../../assets/icons/Arrow 16.svg";
+import { useState } from "react";
 import css from "./HomePage.module.css";
 function HomePage() {
+  const [url, setUrl] = useState("");
+  const [qrSrc, setQrSrc] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    setQrSrc(
+      `https://api.qrserver.com/v1/create-qr-code/?size=220x220&data=${encodeURIComponent(url)}`,
+    );
+  };
+
   return (
     <div className={css.divHome}>
       <div className={css.divContent}>
@@ -24,6 +36,26 @@ function HomePage() {
           srcSet="/images/hero/heroImg2x.jpg 2x"
           alt="Psychologist"
         />
+      </div>
+      <div className={css.qrSection}>
+        <h2 className={css.qrSectionTitle}>Share this website</h2>
+
+        <form onSubmit={handleSubmit}>
+          <input
+            className={css.qrSectionInput}
+            type="text"
+            value={url}
+            onChange={(e) => setUrl(e.target.value)}
+            placeholder="Enter URL to generate QR"
+          />
+          <button className={css.qrSectionBtn} type="submit">
+            Generate QR
+          </button>
+        </form>
+        <p className={css.qrSectionText}>Enter any URL to generate a QR code</p>
+        {qrSrc && (
+          <img className={css.qrSectionImg} src={qrSrc} alt="QR Code" />
+        )}
       </div>
     </div>
   );
